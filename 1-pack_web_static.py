b064f3ac6c11bf4e7bf5b696b8c16056011903af
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-04 17:06:13 +0200

    added 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,8 +10,16 @@ from fabric.api import local, runs_once
 
 @runs_once
 def do_pack():
-    """generates a .tgz archive from the contents of the web_static folder"""
-    local("mkdir -p versions")
-    local("tar -cvzf versions/web_static_$(date '+%Y%m%d%H%M%S').tgz web_static")
-
-    return "versions/web_static_$(date '+%Y%m%d%H%M%S').tgz"
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    output = f"versions/web_static_{time}.tgz"
+    try:
+        print(f"Packing web_static to {output}")
+        local(f"tar -cvzf {output} web_static")
+        size = os.stat(output).st_size
+        print(f"web_static packed: {output} -> {size} Bytes")
+    except Exception:
+        output = None
+    return output
