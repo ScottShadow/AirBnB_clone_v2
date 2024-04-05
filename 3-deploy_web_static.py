49621530a88e7923b88c970fd1a31cca85144b3b
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-05 15:39:57 +0200

    changed ips

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.85.131.209", "100.25.163.221"]
 
 
 def do_pack():
