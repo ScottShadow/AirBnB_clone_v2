33361ec524c350dc7098839b4923a0597eee7153
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-05 15:35:18 +0200

    advanced done

---
+++ 100-clean_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# Fabfile to delete out-of-date archives.
+import os
+from fabric.api import *
+
+env.hosts = ["104.196.168.90", "35.196.46.172"]
+
+
+def do_clean(number=0):
+    """
+    Function to clean out-of-date archives.
+
+    Args:
+        number (int): Number of archives to clean. Defaults to 0.
+    """
+
+    # Convert number to int and set to 1 if 0
+    number = 1 if int(number) == 0 else int(number)
+
+    # Sort archives and remove specified number
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+
+    # Change local directory and remove archives
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    # Change remote directory and remove archives
+    with cd("/data/web_static/releases"):
+        # Get list of archives and filter for web_static_
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        # Remove specified number of archives
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
