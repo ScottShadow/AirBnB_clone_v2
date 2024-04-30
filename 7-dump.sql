8dbccd0c6cc61cef4ce55af7c6e555d8c07dfba1
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-30 12:26:37 +0200

    7-dump modded to accomodate places table

--- 7-dump.sql
+++ 7-dump.sql
@@ -71,6 +71,28 @@ CREATE TABLE `states` (
   `name` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `places`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE places (
+  `id` VARCHAR(60) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `city_id` VARCHAR(60) NOT NULL,
+  `user_id` VARCHAR(60) NOT NULL,
+  `name` VARCHAR(128) NOT NULL,
+  `description` VARCHAR(1024),
+  `number_rooms` INTEGER NOT NULL,
+  `number_bathrooms` INTEGER NOT NULL,
+  `max_guest` INTEGER NOT NULL,
+  `price_by_night` INTEGER NOT NULL,
+  `latitude` FLOAT,
+  `longitude` FLOAT,
+  PRIMARY KEY (id),
+  UNIQUE (id),
+  FOREIGN KEY(city_id) REFERENCES cities (id),
+  FOREIGN KEY(user_id) REFERENCES users (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
